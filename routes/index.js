019de94fac17fd683a3bf033c8186ae765ce46ec
lxl12345 <1757593685@qq.com>
2017-10-25 19:22:19 +0800

    李晓璐10月25日作业

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var fs=require('fs');
+
+/* GET home page. */
+router.post('/lxl', function(req, res, next) {
+	res.header('Access-Control-Allow-Origin','*')//解决跨域
+	var temp=[]
+	switch (req.body.abc){
+		case "1":
+			temp=[1,2,3]
+			break;
+		case "2":
+			temp=[4,5,6]
+			break;
+	}
+	res.send({"name":temp});
+});
+
+module.exports = router;
